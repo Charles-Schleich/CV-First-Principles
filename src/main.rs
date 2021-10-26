f0f8691f28eb1ac898795df290a54a4d88f91993
Charles Schleich <schleich.cdaniel@gmail.com>
2021-10-26 20:56:48 +0200

    added image dep

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-
+use imageproc::window::display_image;
+use std::env;
 
 fn main() {
     println!("Hello, world!");
+
+    let image_path = "images/lena.jpg";
+
+    let image = image::open(&image_path)
+        .expect("No image found at provided path")
+        .to_rgba();
+
+    display_image("", &image, 500, 500);
+
 }
