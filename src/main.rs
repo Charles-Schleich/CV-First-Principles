6cfcfc73d9eacf366ba2750c1c9247624d0d2017
Charles Schleich <schleich.cdaniel@gmail.com>
2021-12-13 21:59:00 +0100

    attempting the red video edit of polyhia, still working on it

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ use binary_proc::binaryproc_main;
 mod filters;
 use filters::filterproc_main;
 
+mod experiments;
+use experiments::polyphia_main;
 
 fn main() {
     println!("Hello, world!");
     
     // binaryproc_main();
-    filterproc_main();
+    // filterproc_main();
     // display_image("", &image, 500, 500);
+    polyphia_main();
 }
